8e3d8e5262d6b5219ab547e378309d903e01c599
Lilian Perim <liliancrisperim@gmail.com>
2023-12-13 17:15:16 -0300

    ajustando o adapter com todos os campos

--- app/src/main/java/br/edu/scl/ifsp/sdm/moviesmanager/adapter/MovieAdapter.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/moviesmanager/adapter/MovieAdapter.kt
@@ -3,41 +3,47 @@ package br.edu.scl.ifsp.sdm.moviesmanager.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import br.edu.scl.ifsp.sdm.moviesmanager.R
 import br.edu.scl.ifsp.sdm.moviesmanager.data.entity.Movie
 import br.edu.scl.ifsp.sdm.moviesmanager.databinding.MovieItemBinding
 
 class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
-    private lateinit var binding: MovieItemBinding
     var moviesList = ArrayList<Movie>()
-    var movieListFilterable = ArrayList<Movie>()
 
-    fun updateList(newList: ArrayList<Movie>) {
-        moviesList = newList
-        movieListFilterable = moviesList
+    fun updateList(newList: List<Movie>) {
+        moviesList.clear()
+        moviesList.addAll(newList)
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): MovieViewHolder {
-        binding = MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding = MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MovieViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        holder.nomeView.text = moviesList[position].nome
-        holder.anoView.text = moviesList[position].ano
-        holder.duracaoView.text = moviesList[position].duracao.toString()
+        val movie = moviesList[position]
+        val context = holder.itemView.context
+        with(holder) {
+            nomeView.text = context.getString(R.string.format_nome, movie.nome)
+            generoView.text = context.getString(R.string.format_genero, movie.genero)
+            anoView.text = context.getString(R.string.format_ano, movie.ano)
+            produtoraView.text = context.getString(R.string.format_produtora, movie.produtora)
+            duracaoView.text = context.getString(R.string.format_duracao, movie.duracao)
+            notaView.text = context.getString(R.string.format_nota, movie.nota.toString())
+            assistidoView.isChecked = movie.assistido
+        }
     }
 
-    override fun getItemCount(): Int {
-        return moviesList.size
-    }
+    override fun getItemCount() = moviesList.size
 
     inner class MovieViewHolder(binding: MovieItemBinding) : RecyclerView.ViewHolder(binding.root) {
         val nomeView = binding.nome
+        val generoView = binding.genero
         val anoView = binding.ano
+        val produtoraView = binding.produtora
         val duracaoView = binding.duracao
+        val notaView = binding.nota
+        val assistidoView = binding.checkboxAssistido
     }
 }
